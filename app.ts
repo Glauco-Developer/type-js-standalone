f76bd73ac06aacf8c24ac5e25e5fc37968ca522b
Glauco Raupp Russo <glauco.developer@gmail.com>
2017-09-02 00:22:45 -0300

    removed semicolons it'snt in typescript

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
-let message: string = "Hello";
-console.log(message + " Dude");
+let message: string = "Hello"
+console.log(message + " Dude")
 
-let nro: number = 10;
+let nro: number = 10
+let nro = '4'
+console.log(nro)
